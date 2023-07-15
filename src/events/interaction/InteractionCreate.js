9833bb345f6e15bebc7929fd25bc68d944ced12a
Nhu Phuc <phucho0237@gmail.com>
2023-07-15 10:13:53 +0700

    fix: :bug: fix the command can only use once

--- src/events/interaction/InteractionCreate.js
+++ src/events/interaction/InteractionCreate.js
@@ -1,23 +1,19 @@
 const { Events, ChatInputCommandInteraction, Client } = require("discord.js");
 
-var colors = require("colors/safe");
-
 module.exports = {
    name: Events.InteractionCreate,
-   once: true,
    /**
     *
     * @param {ChatInputCommandInteraction} interaction
-    * @param {Client} client
     */
-   async execute(interaction, client) {
+   async execute(interaction) {
       if (!interaction.isChatInputCommand()) return;
 
       const command = interaction.client.commands.get(interaction.commandName);
       if (!command) return;
 
       try {
-         await command.execute(interaction, client);
+         await command.execute(interaction);
       } catch (err) {
          console.error(err);
       }
