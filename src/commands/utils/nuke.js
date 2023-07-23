b45f01592f7fedd0ccb50ad8573b041f6fe21df6
Nhu Phuc <phucho0237@gmail.com>
2023-07-23 09:54:50 +0700

    feat: :sparkles: nuke command

---
+++ src/commands/utils/nuke.js
@@ -0,0 +1,39 @@
+const {
+   SlashCommandBuilder,
+   PermissionFlagsBits,
+   ChatInputCommandInteraction,
+   PermissionsBitField,
+   EmbedBuilder
+} = require("discord.js");
+
+module.exports = {
+   data: new SlashCommandBuilder()
+      .setName("nuke")
+      .setDescription("Nuke a channel")
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
+   /**
+    *
+    * @param {ChatInputCommandInteraction} interaction
+    */
+   async execute(interaction) {
+      if (
+         !interaction.member.permissions.has(
+            PermissionsBitField.Flags.ManageChannels
+         )
+      )
+         return interaction.reply({
+            content: "You don't have permission to use this command.",
+            ephemeral: true
+         });
+
+      await interaction.channel.clone().then(c => {
+         c.setPosition(interaction.channel.position).then(
+            interaction.channel.delete()
+         );
+
+         c.send({
+            content: `Nuked by <@${interaction.user.id}>`
+         });
+      });
+   }
+};
