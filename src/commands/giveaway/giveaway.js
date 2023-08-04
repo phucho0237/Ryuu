bb8bd3562390fb2d7d1c7869364657615eed3210
Nhu Phuc <phucho0237@gmail.com>
2023-08-04 10:08:18 +0000

    feat: :sparkles: giveaway command (not complete)

---
+++ src/commands/giveaway/giveaway.js
@@ -0,0 +1,83 @@
+const {
+   SlashCommandBuilder,
+   PermissionFlagsBits,
+   ChatInputCommandInteraction
+} = require("discord.js");
+const ms = require("ms");
+
+module.exports = {
+   data: new SlashCommandBuilder()
+      .setName("giveaway")
+      .setDescription("Create and manage giveaway in the server")
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+      .addSubcommand(subcommand =>
+         subcommand
+            .setName("start")
+            .setDescription("Start the giveaway")
+            .addStringOption(option =>
+               option
+                  .setName("duration")
+                  .setDescription("The duration of the giveaway")
+                  .setRequired(true)
+            )
+            .addIntegerOption(option =>
+               option
+                  .setName("winners")
+                  .setDescription("The number of winners")
+                  .setRequired(true)
+            )
+            .addStringOption(option =>
+               option
+                  .setName("prize")
+                  .setDescription("The prize of the giveaway")
+                  .setRequired(true)
+            )
+      ),
+   /**
+    *
+    * @param {ChatInputCommandInteraction} interaction
+    */
+   async execute(interaction) {
+      if (
+         !interaction.member.permissions.has(PermissionFlagsBits.ManageMessages)
+      )
+         return interaction.reply({
+            content: "You don't have permission to use this command.",
+            ephemeral: true
+         });
+
+      const subcommand = interaction.options.getSubcommand();
+
+      if (subcommand === "start") {
+         const duration = interaction.options.getString("duration");
+         const winners = interaction.options.getInteger("winners");
+         const prize = interaction.options.getString("prize");
+
+         await interaction.client.giveawaysManager.start(interaction.channel, {
+            duration: ms(duration),
+            winnerCount: winners,
+            prize,
+            messages: {
+               giveaway: "",
+               giveawayEnded: "",
+               title: "{this.prize}",
+               drawing: "Drawing: {timestamp}",
+               dropMessage: "Be the first to react with 🎉",
+               inviteToParticipate: "React with 🎉 to participate",
+               winMessage:
+                  "Congratulations, {winners}! You won **{this.prize}**!\n{this.messageURL}",
+               embedFooter: "{this.winnerCount} winner(s)",
+               noWinner: "Giveaway cancelled, no valid participations.",
+               hostedBy: "Hosted by: {this.hostedBy}",
+               winners: "Winner(s):",
+               endedAt: "Ended at"
+            }
+         });
+
+         interaction.reply({
+            content: "Giveaway has started!",
+            ephemeral: true
+         });
+      }
+   }
+};
