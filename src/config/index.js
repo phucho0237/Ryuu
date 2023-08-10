9ea7db95df2c79846705fa936d8db6b777e56997
Nhu Phuc <phucho0237@gmail.com>
2023-08-10 10:01:05 +0000

    feat: :sparkles: ownerOnly commands

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 module.exports = {
    bot: {
       token: process.env.TOKEN,
-      clientId: process.env.CLIENT_ID
+      clientId: process.env.CLIENT_ID,
+      ownerId: ["1120195827038691429"]
    },
    db: {
       url: process.env.MONGO_URL
--- src/events/interaction/InteractionCreate.js
+++ src/events/interaction/InteractionCreate.js
@@ -12,6 +12,15 @@ module.exports = {
       const command = interaction.client.commands.get(interaction.commandName);
       if (!command) return;
 
+      if (command.ownerOnly) {
+         const ownerIDArray = interaction.client.config.bot.ownerId;
+         if (!ownerIDArray.includes(interaction.user.id))
+            await interaction.reply({
+               content: "You don't have permission to use this command.",
+               ephemeral: true
+            });
+      }
+
       try {
          await command.execute(interaction);
       } catch (err) {
