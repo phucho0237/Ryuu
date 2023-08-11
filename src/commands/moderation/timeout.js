0384d4b8e2485b1d2790640fc4776c8284ee2fd8
Nhu Phuc <phucho0237@gmail.com>
2023-08-11 06:59:03 +0000

    feat: :sparkles: timeout command

---
+++ src/commands/moderation/timeout.js
@@ -0,0 +1,68 @@
+const {
+   SlashCommandBuilder,
+   PermissionFlagsBits,
+   ChatInputCommandInteraction
+} = require("discord.js");
+const ms = require("ms");
+
+module.exports = {
+   data: new SlashCommandBuilder()
+      .setName("timeout")
+      .setDescription("Timeout a user")
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageGuild)
+      .addUserOption(option =>
+         option
+            .setName("user")
+            .setDescription("The user you want to timeout")
+            .setRequired(true)
+      )
+      .addStringOption(option =>
+         option
+            .setName("time")
+            .setDescription("The time you want for the timeout")
+            .setRequired(true)
+      )
+      .addStringOption(option =>
+         option.setName("reason").setDescription("The reason for the timeout")
+      ),
+   /**
+    *
+    * @param {ChatInputCommandInteraction} interaction
+    */
+   async execute(interaction) {
+      const user = interaction.options.getMember("user");
+      const time = interaction.options.getString("time");
+      const reason =
+         interaction.options.getString("reason") || "No reason provided";
+
+      if (!interaction.member.permissions.has(PermissionFlagsBits.ManageGuild))
+         return interaction.reply({
+            content: "You don't have permission to use this command.",
+            ephemeral: true
+         });
+
+      if (
+         !time.endsWith("s") &&
+         !time.endsWith("m") &&
+         !time.endsWith("h") &&
+         !time.endsWith("h")
+      )
+         return interaction.reply({
+            content: "The time you provided must be like this: 1s, 2m, 3h,...",
+            ephemeral: true
+         });
+
+      try {
+         await user.timeout(ms(time), reason);
+         interaction.reply({
+            content: `Successfully timeout <@${user.id}> for \`${time}\``
+         });
+      } catch (err) {
+         interaction.reply({
+            content:
+               "There was a problem when trying to execute this command. Please try again later."
+         });
+         console.error(err);
+      }
+   }
+};
