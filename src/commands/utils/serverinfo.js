dc49ebab5f797ca202a2e663a49d1397b4971c61
Nhu Phuc <phucho0237@gmail.com>
2023-08-16 03:26:00 +0000

    feat: :sparkles: serverinfo

---
+++ src/commands/utils/serverinfo.js
@@ -0,0 +1,87 @@
+const {
+   SlashCommandBuilder,
+   PermissionFlagsBits,
+   ChatInputCommandInteraction,
+   EmbedBuilder,
+   ChannelType
+} = require("discord.js");
+
+module.exports = {
+   data: new SlashCommandBuilder()
+      .setName("serverinfo")
+      .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
+      .setDescription("Get information about this server"),
+   /**
+    *
+    * @param {ChatInputCommandInteraction} interaction
+    */
+   async execute(interaction) {
+      interaction.reply({
+         embeds: [
+            new EmbedBuilder()
+               .setColor("#6AD9F3")
+               .setAuthor({
+                  name: interaction.guild.name,
+                  iconURL: interaction.guild.iconURL({ size: 256 })
+               })
+               .addFields(
+                  {
+                     name: "Owner",
+                     value: `<@${
+                        (await interaction.guild.fetchOwner()).user.id
+                     }>`,
+                     inline: true
+                  },
+                  {
+                     name: "Category Channels",
+                     value: `${
+                        interaction.guild.channels.cache
+                           .filter(c => c.type === ChannelType.GuildCategory)
+                           .toJSON().length
+                     }`,
+                     inline: true
+                  },
+                  {
+                     name: "Text Channels",
+                     value: `${
+                        interaction.guild.channels.cache
+                           .filter(c => c.type === ChannelType.GuildText)
+                           .toJSON().length
+                     }`,
+                     inline: true
+                  },
+                  {
+                     name: "Voice Channels",
+                     value: `${
+                        interaction.guild.channels.cache
+                           .filter(c => c.type === ChannelType.GuildVoice)
+                           .toJSON().length
+                     }`,
+                     inline: true
+                  },
+                  {
+                     name: "Members",
+                     value: `${interaction.guild.memberCount}`,
+                     inline: true
+                  },
+                  {
+                     name: "Roles",
+                     value: `${interaction.guild.roles.cache.size - 1}`,
+                     inline: true
+                  },
+                  {
+                     name: "Roles List",
+                     value: `${interaction.guild.roles.cache
+                        .toJSON()
+                        .join(", ")}`
+                  }
+               )
+               .setFooter({
+                  text: `Server ID: ${
+                     interaction.guild.id
+                  } | Created At: ${interaction.guild.createdAt.toDateString()}`
+               })
+         ]
+      });
+   }
+};
