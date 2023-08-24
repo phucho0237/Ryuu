eabf87bb0ca3a8b1b7b4762d6a984f32abe1f98d
Nhu Phuc <phucho0237@gmail.com>
2023-08-24 22:52:36 +0000

    fix: :bug: fix color issue

--- src/handlers/antiCrash.js
+++ src/handlers/antiCrash.js
@@ -1,14 +1,18 @@
+var colors = require("colors/safe");
+
 module.exports = client => {
    process.on("unhandledRejection", (reason, p) => {
-      console.log("[AntiCrash] | Unhandled Rejection/Catch".red);
+      console.log(colors.red("[AntiCrash] | Unhandled Rejection/Catch"));
       console.log(reason, p);
    });
    process.on("uncaughtException", (err, origin) => {
-      console.log("[AntiCrash] | Uncaught Exception/Catch".red);
+      console.log(colors.red("[AntiCrash] | Uncaught Exception/Catch"));
       console.log(err, origin);
    });
    process.on("uncaughtExceptionMonitor", (err, origin) => {
-      console.log("[AntiCrash] | Uncaught Exception/Catch (Monitor)".red);
+      console.log(
+         colors.red("[AntiCrash] | Uncaught Exception/Catch (Monitor)")
+      );
       console.log(err, origin);
    });
 };
