2b82b8c74c4304aa806155d2c75c4fe06ae41cbc
Nhu Phuc <phucho0237@gmail.com>
2023-09-06 06:31:55 +0000

    feat: :sparkles: stop & skip command

--- src/commands/music/music.js
+++ src/commands/music/music.js
@@ -3,7 +3,7 @@ const {
    PermissionFlagsBits,
    ChatInputCommandInteraction
 } = require("discord.js");
-const { useMainPlayer } = require("discord-player");
+const { useMainPlayer, useQueue } = require("discord-player");
 
 module.exports = {
    data: new SlashCommandBuilder()
@@ -13,13 +13,19 @@ module.exports = {
       .addSubcommand(subcommand =>
          subcommand
             .setName("play")
-            .setDescription("Play a music/playlist")
+            .setDescription("Play a song/playlist")
             .addStringOption(option =>
                option
                   .setName("query")
                   .setDescription("The query of a song/playlist")
                   .setRequired(true)
             )
+      )
+      .addSubcommand(subcommand =>
+         subcommand.setName("stop").setDescription("Stop the player")
+      )
+      .addSubcommand(subcommand =>
+         subcommand.setName("skip").setDescription("Skip a song")
       ),
    /**
     *
@@ -32,23 +38,49 @@ module.exports = {
 
       const channel = interaction.member.voice.channel;
 
-      const player = useMainPlayer();
-
       if (!channel)
          return interaction.reply({
             content: "You need to join a voice channel first.",
             ephemeral: true
          });
 
+      const player = useMainPlayer();
+      const queue = useQueue(interaction.guild.id);
+
       if (subcommand === "play") {
          const query = interaction.options.getString("query");
 
+         const searchResult = await player.search(query, {
+            requestedBy: interaction.user
+         });
+         if (!searchResult.hasTracks)
+            return interaction.editReply({
+               content: `No track found for \`${query}\``
+            });
+
          try {
-            const { track } = await player.play(channel, query, {
+            const { track } = await player.play(channel, searchResult, {
                nodeOptions: { metadata: interaction }
             });
 
             return interaction.followUp(`\`${track.title}\` enqueued`);
+         } catch (err) {
+            console.error(err);
+         }
+      } else if (subcommand === "stop") {
+         try {
+            queue.clear();
+            queue.node.stop();
+            interaction.followUp({
+               content: "Stopped the queue"
+            });
+         } catch (err) {
+            console.error(err);
+         }
+      } else if (subcommand === "skip") {
+         try {
+            queue.node.skip();
+            interaction.followUp("Successfully skipped");
          } catch (err) {
             console.error(err);
          }
